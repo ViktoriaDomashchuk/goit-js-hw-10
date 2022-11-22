f368452b3bb4086369e80da00197699e1b5b27d1
Viktoria Domashchuk <domashchukviktoria@gmail.com>
2022-11-22 09:34:56 +0200

    add libraries

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import './css/styles.css';
-
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix';
+console.log(Notify);
 const DEBOUNCE_DELAY = 300;
+const refs = {
+  input: document.querySelector('#search-box'),
+};
+console.log(refs.input);
